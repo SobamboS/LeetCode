159132c38cd71856e38cc8d8cf98f18866b2d534
SobamboS <sobamboolusola1152@gmail.com>
2022-11-22 11:55:20 +0100

    TwoSum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,3 +1,18 @@
 public class TwoSum{
+    public static void main(String[] args){
+        int[] nums={1,2,3,4,5,6};
+        int target;
+    }
 
+    public int[] twoSum(int[]nums,int target){
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[j] == target - nums[i]){
+                    return new int[]{i,j};
+
+                }
+            }
+        }
+        return null;
+    }
 }
