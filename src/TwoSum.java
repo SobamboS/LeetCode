7c44ea4e8f3586711554b48fd6727149ea1b96c5
SobamboS <sobamboolusola1152@gmail.com>
2022-11-22 13:20:07 +0100

    TwoSum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,10 +1,14 @@
+import java.util.Arrays;
+
 public class TwoSum{
     public static void main(String[] args){
-        int[] nums={1,2,3,4,5,6};
-        int target;
+        int[] nums={3,2,3,4,5,6};
+        int target=6;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+
     }
 
-    public int[] twoSum(int[]nums,int target){
+    public static int[] twoSum(int[] nums,int target){
         for(int i = 0; i < nums.length; i++){
             for(int j = i + 1; j < nums.length; j++){
                 if(nums[j] == target - nums[i]){
